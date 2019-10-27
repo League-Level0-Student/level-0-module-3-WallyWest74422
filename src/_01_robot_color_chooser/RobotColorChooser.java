5493a5573e10d4178579d553d280dc1aaac84367
League Student <league@iMac-37.attlocal.net>
2019-10-27 15:57:08 -0700

    hi

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,24 +4,33 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot zane = new Robot("batman");
+		zane.miniaturize();
+		zane.sparkle();
+		zane.setSpeed(200);
+		zane.penDown();
 		//3. Ask the user what color they would like the robot to draw
-		
+		String color = JOptionPane.showInputDialog("What color would you like me to draw? Green Blue or Purple");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+if(color.equals("blue"))
         //6. If the user doesn’t enter anything, choose a random color
 
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
-		
+		zane.setPenWidth(10);
 	    //2. Make the robot draw a shape (this will take more than one line of code)
+for(int i=0; i<3; i++) {
+zane.move(100);
+zane.turn(120);
 
-
-	}
+	}}
 }
